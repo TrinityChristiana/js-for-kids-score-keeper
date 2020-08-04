6184d5b23a4dd1704bb5614e8e5688c1e313c370
Trinity Terry <trinityterry@Trinitys-MacBook-Pro.local>
2020-08-04 11:38:19 -0500

    added delete

--- scripts.js
+++ scripts.js
@@ -1,11 +1,5 @@
 //This holds the default names and scores when the DOM is refreshed
-let scores = {
-  Alyssa: 0,
-  Demetre: 0,
-  DJ: 0,
-  Seven: 0,
-  Trinity: 0,
-};
+const scoreArray = [['Team 1', 0]];
 
 // Assigns keys of default object to an array
 const scoreDiv = document.querySelector('#scores');
@@ -13,74 +7,73 @@ const pointsDiv = document.querySelector('#points');
 
 // Writes Welcome message
 scoreDiv.innerHTML = `<p class="display-1">Scores Will Show Up Here!</p>
-<h2>Click name to edit it</h2>
-<h2>Scores can not go less than zero</h2>
-<h2>Names cannot have more than 8 characters.</h2>
-<h2>Enjoy!</h2>`;
-
-// Show's Button's Name and hide's Name Input; Update's Names using User's Input; Validates Input; Updates Score Names;
-const getNameInput = (person, content, input) => {
-  const name = document.querySelector(person).value;
-  const i = person.slice(-1);
-  const oldName = Object.keys(scores)[i];
-  const oldScore = scores[oldName];
-  const validationDiv = document.querySelector(`#inputValid${i}`);
-  const inputDiv = document.querySelector(input);
-  const contentDiv = document.querySelector(content);
-  if (name.length > 8) {
-    validationDiv.innerHTML = `<p>Maximum of 8 Characters</p>`;
-  } else if (name !== '') {
-    contentDiv.innerHTML = name;
-    delete scores[oldName];
-    scores[name] = oldScore;
-    inputDiv.style.display = 'none';
-    contentDiv.style.display = 'block';
-    showScores();
-    validationDiv.innerHTML = ``;
-  } else {
-    inputDiv.style.display = 'none';
-    contentDiv.style.display = 'block';
-    showScores();
-    validationDiv.innerHTML = ``;
-  }
-};
+  <h2>Click name to edit it</h2>
+  <h2>Scores can not go less than zero</h2>
+  <h2>Names cannot have more than 8 characters.</h2>
+  <h2>Enjoy!</h2>`;
 
 //Writes Buttons and Button's names to DOM
 const getPointText = () => {
   pointsDiv.innerHTML = '';
-  Object.keys(scores).forEach(function (key, i) {
+  scoreArray.forEach(function ([name, score], i) {
     pointsDiv.innerHTML += `
-    <div class="friend">
-        <div class="input-group mb-3" id="input${i}" style="display:none">
-            <div class="input-group-prepend" >
-                <button type="submit" class="btn btn-outline-secondary" value=${i} id="personButton${i}">edit</button>
-            </div>
-            <input type="text" id="personInput${i}" class="form-control" placeholder="" aria-label="name" aria-describedby="button-addon1">
-        
-        </div>
-        <div class="validate" id="inputValid${i}"></div>
-        <h2 id="person${i}">${key}</h2>
-        <button type="submit" class="btn btn-outline-success" id="plus${i}">
-            <h2>+1</h2>
-        </button>
-        <button type="submit" class="btn btn-outline-danger" id="minus${i}" disabled>
-            <h2>-1</h2>
-        </button>
-    </div>
-`;
+      <div class="friend">
+          <div class="input-group mb-3" id="input${i}" style="display:none">
+              <div class="input-group-prepend" >
+                  <button type="submit" class="btn btn-outline-secondary" value=${i} id="personButton${i}">edit</button>
+              </div>
+              <input type="text" id="personInput${i}" class="form-control" placeholder="" aria-label="name" aria-describedby="button-addon1">
+          
+          </div>
+          <div class="validate" id="inputValid${i}"></div>
+          <h2 id="person${i}">     
+          ${name} </h2> 
+          <button type="submit" class="btn btn-outline-success" id="plus${i}">
+              <h2>+1</h2>
+          </button>
+          <button type="submit" class="btn btn-outline-danger" id="minus${i}" disabled>
+              <h2>-1</h2>
+          </button>
+        <p id="delete${i}" class="delete-button">Delete</p>
+          
+      </div>
+  `;
   });
 
   pointsDiv.innerHTML +=
     '<button type="submit" class="btn btn-outline-secondary" id="add-counter">Add Person</button>';
 };
 
+// Show's Button's Name and hide's Name Input; Update's Names using User's Input; Validates Input; Updates Score Names;
+const getNameInput = (person, content, input) => {
+  const name = document.querySelector(person).value;
+  const i = person.slice(-1);
+  const validationDiv = document.querySelector(`#inputValid${i}`);
+  const inputDiv = document.querySelector(input);
+  const contentDiv = document.querySelector(content);
+  validationDiv.innerHTML = ``;
+  if (name.length > 8) {
+    validationDiv.innerHTML = `<p>Maximum of 8 Characters</p>`;
+  } else if (name !== '') {
+    contentDiv.innerHTML = name;
+    scoreArray[i][0] = name;
+    inputDiv.style.display = 'none';
+    contentDiv.style.display = 'block';
+    showScores();
+  } else {
+    inputDiv.style.display = 'none';
+    contentDiv.style.display = 'block';
+    showScores();
+  }
+};
+
 // Writes Score's Names and scores to DOM
 const showScores = () => {
   scoreDiv.innerHTML = '';
-  Object.keys(scores).forEach((key, i) => {
+  scoreArray.forEach(([name, score], i) => {
     scoreDiv.innerHTML += `<p class='display-3'>
-      ${key}: ${scores[key]}
-    </p>`;
+        ${name}: ${score}
+      </p>`;
   });
 };
 
@@ -91,7 +84,7 @@ const showInput = (content, input) => {
 };
 
 const addEventListeners = () => {
-  Object.keys(scores).forEach(function (key, i) {
+  scoreArray.forEach(function ([name, score], i) {
     const nameInputFunction = () =>
       getNameInput(`#personInput${i}`, `#person${i}`, `#input${i}`);
 
@@ -119,29 +112,41 @@ const addEventListeners = () => {
 
     const plusButton = document.querySelector(`#plus${i}`);
     const minusButton = document.querySelector(`#minus${i}`);
+    const deleteButton = document.querySelector(`#delete${i}`);
 
     // Add one Point Button
     plusButton.addEventListener('click', () => {
-      scores[key] += 1;
+      scoreArray[i][1] += 1;
+      console.log(scoreArray[i][1]);
       showScores();
-      if (scores[key] > 0) {
+      if (scoreArray[i][1] > 0) {
         minusButton.disabled = false;
       }
     });
 
     //Minus one Point Button
     minusButton.addEventListener('click', () => {
-      scores[key] -= 1;
+      scoreArray[i][1] -= 1;
       showScores();
-      if (scores[key] == 0) {
+      if (scoreArray[i][1] == 0) {
         minusButton.disabled = true;
+      }
+    });
+
+    deleteButton.addEventListener('click', () => {
+      if (window.confirm(`Are you sure you want to delete ${name}`)) {
+        scoreArray.splice(i, 1);
+        runIt();
+        showScores();
       }
     });
   });
 
   const addCounterBtn = document.querySelector(`#add-counter`);
   addCounterBtn.addEventListener('click', () => {
-    console.log('clicked it');
+    scoreArray.push(['new counter', 0]);
+    runIt();
+    showScores();
   });
 };
 
@@ -149,9 +154,5 @@ const runIt = () => {
   getPointText();
   addEventListeners();
 };
-
-runIt();
-
-scores.test = 0;
 
 runIt();
